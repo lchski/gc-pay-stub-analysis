232668c4099621b0dee6f4cb7be123b6ae2f9a7b
Lucas Cherkewski <lucas@lucascherkewski.com>
2020-11-13 11:15:03 -0500

    load stub data

--- load.R
+++ load.R
@@ -1,5 +1,14 @@
 library(tidyverse)
 library(readxl)
 library(janitor)
+library(lubridate)
 
+stubs <- read_excel("data/source/Pay stubs.xlsx", sheet = "summary") %>%
+  clean_names() %>%
+  mutate(date = as_date(date)) %>%
+  mutate_at(vars(total_taxes_and_deductions, tax:disability_insurance), ~ .x * -1)
 
+entitlements_deductions <- read_excel("data/source/Pay stubs.xlsx", sheet = "entitlements_deductions") %>%
+  clean_names() %>%
+  mutate_at(vars(contains("date")), dmy) %>%
+  mutate(dates = date_start %--% date_end)
